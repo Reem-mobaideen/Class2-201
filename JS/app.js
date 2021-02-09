dbc78307456496c8ebb61e99dd178abc391cc527
Reem-mobaideen <mubaideen.reem@gmail.com>
2021-02-09 16:26:27 +0200

    array

--- JS/app.js
+++ JS/app.js
@@ -10,38 +10,46 @@ let winter=prompt('Do you like rain?');
 let day=prompt("what's your faviourite day?");
 let color=prompt("what's your faviourite color?");
 let sport=prompt('Do you like playing basketball?');
+let guess=prompt('try to guess my age?');
+
+
 
 let score=0;
 
-story=story.toLowerCase();
-winter=winter.toLowerCase();
-day=day.toLowerCase();
-color=color.toLowerCase();
-sport=sport.toLowerCase();
-
-if (story==='yes'){
+if (story.toLowerCase()==='yes'||story.toLowerCase()==='y'){
     score=score+5;
 }
-if (winter=== 'yes'){
+if (winter.toLowerCase()==='no'||winter.toLowerCase()==='n'){
     score=score+5;
 }
-if (day=== 'yes'){
+if (day.toLowerCase()==='yes'||day.toLowerCase()==='y'){
     score=score+5;
 }
-if(color==='yes'){
-    score=score+5
+if(color.toLowerCase()==='yes'||color.toLowerCase()==='y'){
+    score=score+5;
 }
-if (sport==='yes'){
+if (sport.toLowerCase()==='yes'||sport.toLowerCase()==='y'){
 score=score+5;
+if (guess >=40){
+    alert('the number of guessing is too high');
+} else{
+    alert('the number of guessing is too low');
+}
 }
 console.log(score);
+alert ('you score is '+ score);
 
+let answer=['Cultural',' Camping','adventure','medical','cruise','international', 'buisness'];
+let fun=prompt("what's types of tourism you prefer?");
+for(let i =0; i<answer.length; i++){
+if(fun.toLowerCase()==='cultural'|| fun.toLowerCase()==='advanture'|| fun.toLowerCase()=== 'buisness'){
+    console.log(answer[i]);
+     fun=prompt("try again?");
+}
+else {break;
 
-/*switch(story){
-        case true:
-            document.write('');
-           break;
-       case false:
-           document.write('I hope you get better');
-             break;
-console.log(fruit.toUpperCase());*/
+    console.log(answer[i]);
+    
+}
+}
+alert(answer[0]+'  '+ answer[2]+'  '+ answer[6]);
