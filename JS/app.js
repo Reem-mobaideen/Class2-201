dd8c7c550fe82273b735036275ac412489cbf174
Rawan199812 <rawan.13579@icloud.com>
2021-02-10 17:28:46 +0200

    adding functions

--- JS/app.js
+++ JS/app.js
@@ -10,12 +10,12 @@ let winter=prompt('Do you like rain?');
 let day=prompt("what's your faviourite day?");
 let color=prompt("what's your faviourite color?");
 let sport=prompt('Do you like playing basketball?');
-let guess=prompt('try to guess my age?');
+
 
 
 
 let score=0;
-
+function yesorno(){
 if (story.toLowerCase()==='yes'||story.toLowerCase()==='y'){
     score=score+5;
 }
@@ -30,17 +30,23 @@ if(color.toLowerCase()==='yes'||color.toLowerCase()==='y'){
 }
 if (sport.toLowerCase()==='yes'||sport.toLowerCase()==='y'){
 score=score+5;
+} yesorno();
+let guess=prompt('try to guess my age?');
+function number(){
 if (guess >=40){
     alert('the number of guessing is too high');
 } else{
     alert('the number of guessing is too low');
 }
 }
+}
+
 console.log(score);
 alert ('you score is '+ score);
 
 let answer=['Cultural',' Camping','adventure','medical','cruise','international', 'buisness'];
 let fun=prompt("what's types of tourism you prefer?");
+function choose(){
 for(let i =0; i<answer.length; i++){
 if(fun.toLowerCase()==='cultural'|| fun.toLowerCase()==='advanture'|| fun.toLowerCase()=== 'buisness'){
     console.log(answer[i]);
@@ -52,4 +58,5 @@ else {break;
     
 }
 }
+}choose();
 alert(answer[0]+'  '+ answer[2]+'  '+ answer[6]);
