cbd4928ffa9aaf0c60c6e460a632d082ef7d9879
Reem-mobaideen <mubaideen.reem@gmail.com>
2021-02-26 20:25:25 +0200

    finishing 05b

--- JS/app.js
+++ JS/app.js
@@ -7,8 +7,8 @@ alert('the score will appear after finish');
 
 let story=prompt('Do you like reading stories?');
 let winter=prompt('Do you like rain?');
-let day=prompt("what's your faviourite day?");
-let color=prompt("what's your faviourite color?");
+let day=prompt("do you like friday?");
+let color=prompt("do you like green color?");
 let sport=prompt('Do you like playing basketball?');
 
 
@@ -17,20 +17,29 @@ let sport=prompt('Do you like playing basketball?');
 let score=0;
 function yesorno(){
 if (story.toLowerCase()==='yes'||story.toLowerCase()==='y'){
-    score=score+5;
+    score++;
 }
 if (winter.toLowerCase()==='no'||winter.toLowerCase()==='n'){
-    score=score+5;
+    score++;
 }
 if (day.toLowerCase()==='yes'||day.toLowerCase()==='y'){
-    score=score+5;
+    score++;
 }
 if(color.toLowerCase()==='yes'||color.toLowerCase()==='y'){
-    score=score+5;
+    score++;
 }
 if (sport.toLowerCase()==='yes'||sport.toLowerCase()==='y'){
-score=score+5;
-} yesorno();
+score++;
+
+} else{
+    
+    alert('the game is finish');
+}
+}
+yesorno();
+
+
+
 let guess=prompt('try to guess my age?');
 function number(){
 if (guess >=40){
@@ -38,18 +47,21 @@ if (guess >=40){
 } else{
     alert('the number of guessing is too low');
 }
+
 }
-}
+
+
 
 console.log(score);
 alert ('you score is '+ score);
 
 let answer=['Cultural',' Camping','adventure','medical','cruise','international', 'buisness'];
 let fun=prompt("what's types of tourism you prefer?");
+
 function choose(){
-for(let i =0; i<answer.length; i++){
-if(fun.toLowerCase()==='cultural'|| fun.toLowerCase()==='advanture'|| fun.toLowerCase()=== 'buisness'){
-    console.log(answer[i]);
+for(let i =0; i<4; i++){
+if(answer.toLowerCase()==='cultural'&& answer.toLowerCase()==='advanture'&& answer.toLowerCase()=== 'buisness'){
+    console.log('the answer is' + answer[i]);
      fun=prompt("try again?");
 }
 else {break;
@@ -57,6 +69,8 @@ else {break;
     console.log(answer[i]);
     
 }
+
 }
-}choose();
+choose();
+}
 alert(answer[0]+'  '+ answer[2]+'  '+ answer[6]);
